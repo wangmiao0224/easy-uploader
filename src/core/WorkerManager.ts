b6569ecda64ab43bd762b46b89b28183cdae6d40
我建议滑着走 <email@sample.com>
2022-05-14 15:33:51 +0800

    chore: 修改文档

--- src/core/WorkerManager.ts
+++ src/core/WorkerManager.ts
@@ -1,18 +1,36 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+//默认参数
 const DEFAULT_MAX_WORKER_COUNT = 6;
+
+/**
+ * worker的运行状态
+ * 0-等待
+ * 1-运行中
+ */
 enum STATE {
   PENDING = 0,
   RUNNING = 1,
 }
+//队列中的worker类型
 type WorksType = {
     id:number
   state: STATE;
   worker: Worker;
 };
-class WorkerManager {
+
+interface WorkerManagerImp { 
+  maxWorkerCount: number; //最大运行数
+  Worker: any;            //实际Worker
+  works: WorksType[];     //worker队列
+  postMessage: (data:any) => Promise<any> //向worker发送请求，并通过Promise接收返回消息
+  terminate:() => void      //关闭所有的Worker
+}
+class WorkerManager implements WorkerManagerImp {
   maxWorkerCount: number;
   Worker: any;
   works: WorksType[] = [];
-  constructor(worker: any, count = DEFAULT_MAX_WORKER_COUNT) {
+  constructor(worker: Worker, count = DEFAULT_MAX_WORKER_COUNT) {
     this.maxWorkerCount = count;
     this.Worker = worker;
       this.works = new Array(this.maxWorkerCount).fill(1).map((v, i) => { 
@@ -23,7 +41,12 @@ class WorkerManager {
            }
       })
   }
-  postMessage(data: any): Promise<any> {
+  /**
+   * worker调用规则：先获取未运行状态worker，如果全部都在运行状态，则随机调用worker
+   * @param data 
+   * @returns 
+   */
+  postMessage(data: any): Promise<MessageEvent> {
     return new Promise((resolve) => {
       const worker =
         this.works.filter((wk) => wk.state === STATE.PENDING)[0] ||
